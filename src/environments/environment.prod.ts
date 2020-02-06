a2f8e7f116b3eac84e80ec4650ade25a6987678a
pantharev <narev001@fiu.edu>
2020-02-06 18:12:34 -0500

    no port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://lms-fiu.herokuapp.com/:18314'
+  apiURL: 'https://lms-fiu.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://lms-fiu.herokuapp.com/:18314'
+  apiURL: 'https://lms-fiu.herokuapp.com/'
 };
 
 /*

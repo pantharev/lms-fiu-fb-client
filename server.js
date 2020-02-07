59975b950c287ea7ff0accffa757d78536f892f8
pantharev <narev001@fiu.edu>
2020-02-07 14:09:31 -0500

    favicon and title update

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const path = require("path");
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/angular-build'));
 app.use(cors());
 app.get('/*', function(req,res){
 res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => {
-    console.log("angular server started on port: " + process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log("angular server started on port: " + port);
 });

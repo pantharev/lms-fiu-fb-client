f01b8d35af84b6131a92d8693e9947cf8bae0c71
pantharev <narev001@fiu.edu>
2020-03-04 14:24:53 -0500

    minor updates for user roles

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -27,7 +27,6 @@ export class AuthenticationService implements CanActivate {
    login(email: string, password: string) {
      return this.http.post<any>(`${environment.apiURL}/auth/login`, { email, password })
             .pipe(map(user => {
-              alert("login");
               // store user details and jwt token in local storage to keep user logged in between page refreshes
               localStorage.setItem('currentUser', JSON.stringify(user));
               this.currentUserSubject.next(user);

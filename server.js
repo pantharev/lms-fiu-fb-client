2f839daaa0f9e692c27bdaca4c087d9cc362a1be
jwats057 <46846829+jwats057@users.noreply.github.com>
2020-03-04 20:49:19 -0500

    signed request test 2

--- server.js
+++ server.js
@@ -12,10 +12,37 @@ app.get('/*', function (req, res) {
 
 
 app.post('/', function (req, res) {
+    console.log("Test1");
     res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
-    console.log("Test");
+    console.log("Test2");
     console.log(req);
+
+    //parse_signed_request(req);
 });
+
+function parse_signed_request(signed_request, secret) {
+    encoded_data = signed_request.split('.', 2);
+    // decode the data
+    sig = encoded_data[0];
+    json = base64url.decode(encoded_data[1]);
+    data = JSON.parse(json); // ERROR Occurs Here!
+
+    // check algorithm - not relevant to error
+    if (!data.algorithm || data.algorithm.toUpperCase() != 'HMAC-SHA256') {
+        console.error('Unknown algorithm. Expected HMAC-SHA256');
+        return null;
+    }
+
+    // check sig - not relevant to error
+    expected_sig = crypto.createHmac('sha256', secret).update(encoded_data[1]).digest('base64').replace(/\+/g, '-').replace(/\//g, '_').replace('=', '');
+    if (sig !== expected_sig) {
+        console.error('Bad signed JSON Signature!');
+        return null;
+    }
+
+    return data;
+}
+
 
 app.all(() => {
     res.header('Access-Control-Allow-Origin', '*'); // your website
--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -8,18 +8,18 @@ import { HeaderComponent } from '@app/shared/header/header.component';
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild('header', { static: false}) private myHeader: HeaderComponent;
+  @ViewChild('header', { static: false }) private myHeader: HeaderComponent;
 
   constructor(private cookieService: CookieService) { }
 
-  getCookie(key: string){
+  getCookie(key: string) {
     return this.cookieService.get(key);
   }
 
   ngOnInit() {
     var userCookie = this.getCookie('user');
     //console.log("The user cookie is: " + this.getCookie('user'));
-    if(localStorage.getItem('user') == '')
+    if (localStorage.getItem('user') == '')
       localStorage.setItem('user', userCookie);
     this.cookieService.delete('user');
     console.log("User: " + localStorage.getItem('currentUser'));

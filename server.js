d429d5ce090d0a66fe3f9a268fca3aadf5affb4f
jwats057 <46846829+jwats057@users.noreply.github.com>
2020-04-07 14:35:35 -0400

    removed nodemailer from frontend

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 8080;
 const _ = require("lodash");
 const base64url = require("base64-url");
-const nodemailer = require("nodemailer");
 var userData;
 
 
@@ -22,28 +21,6 @@ const https = require("https");
 const httpsServer = https.createServer(credentials, app);
 const request = require('request-promise');
 
-const sendMail = (user, callback) => {
-    console.log("in sendMail function");
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'lmsfiustudent@gmail.com', 
-            pass: 'lms@student@fiu' 
-        }
-    });
-
-    const mailOptions = {
-        from: `"FIU LMS", "no-reply-fiu-lms@fiu.edu"`,
-        to: `<jwats057@fiu.edu>`,
-        subject: "LMS TEST",
-        html: "<h1>And here is the place for HTML</h1>"
-    };
-    transporter.sendMail(mailOptions, callback);
-
-}
-
 
 
 
@@ -55,21 +32,6 @@ app.use(cookieParser());
 
 app.get('/userdata', function (req, res) {
     res.send(req.cookies.userData);
-});
-
-app.post("/sendmail", (req, res) => {
-    console.log("server.js request came");
-    let user = req.body;
-    sendMail(user, (err, info) => {
-        if (err) {
-            console.log(err);
-            res.status(400);
-            res.send({ error: "Failed to send email" });
-        } else {
-            console.log("Email has been sent");
-            res.send(info);
-        }
-    });
 });
 
 app.post('/', function (req, res) {
